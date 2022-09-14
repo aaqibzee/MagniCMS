881151bd29b78a7282f88a1ac66cfc0666e1f973
aaqib@guestsmiles.com <aaqib@guestsmiles.com>
2022-09-14 16:12:21 +0500

    Added student table and its  migration

---
+++ MagniCollegeManagementSystem/DatabseContexts/MagniDBContext.cs
@@ -0,0 +1,14 @@
+﻿using MagniCollegeManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MagniCollegeManagementSystem.DatabseContexts
+{
+    public class MagniDBContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
