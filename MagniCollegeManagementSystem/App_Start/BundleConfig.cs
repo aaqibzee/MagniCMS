5ce17f163b7440e54265d48aec4cded68236c200
aaqib@guestsmiles.com <aaqib@guestsmiles.com>
2022-09-17 21:34:36 +0500

    Added bundle config

---
+++ MagniCollegeManagementSystem/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MagniCollegeManagementSystem.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap").Include(
+                "~/Scripts/jquery-3.6.0.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/js/common_scripts.js"));
+
+            //bundles.Add(new StyleBundle(StyleBundleKeys.AutoComplete).Include(
+            //            "~/css/autocomplete.css"));
+
+        }
+    }
+}
