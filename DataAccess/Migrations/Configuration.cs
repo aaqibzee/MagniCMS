ac436baae2aab895fc11e497ce9e6ee2783e3b00
aaqib@guestsmiles.com <aaqib@guestsmiles.com>
2022-09-29 21:44:28 +0500

    Moved Migration Data to Data Access

--- DataAccess/Migrations/Configuration.cs
+++ DataAccess/Migrations/Configuration.cs
@@ -1,11 +1,8 @@
-﻿namespace MagniCollegeManagementSystem.Migrations
+﻿
+using DataAccess.DatabseContexts;
+using System.Data.Entity.Migrations;
+namespace MagniCollegeManagementSystem.Migrations
 {
-    using DataAccess.DatabseContexts;
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-
     internal sealed class Configuration : DbMigrationsConfiguration<MagniDBContext>
     {
         public Configuration()
